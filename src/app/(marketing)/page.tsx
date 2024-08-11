f60e245e4032d14b76064707d01a9b870f6417fa
Richard Kunkli <richard@kunkli.com>
2024-08-11 21:49:59 +0200

    new colorway, fix video

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -76,6 +76,7 @@ const HomePage = () => {
                   height={1200}
                   className="rounded-md lg:rounded-xl bg-foreground/10 shadow-2xl ring-1 ring-border"
                   autoPlay
+                  playsInline
                   muted
                 />
 
@@ -86,8 +87,8 @@ const HomePage = () => {
         </Container>
       </Wrapper>
       <Wrapper className="flex flex-col items-center justify-center py-12 relative">
-        <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-primary rounded-full blur-[10rem] -z-10"></div>
-        <div className="hidden md:block absolute bottom-0 -left-1/3 w-72 h-72 bg-indigo-600 rounded-full blur-[10rem] -z-10"></div>
+        <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-red-700 rounded-full blur-[10rem] -z-10"></div>
+        <div className="hidden md:block absolute bottom-0 -left-1/3 w-72 h-72 bg-amber-600 rounded-full blur-[10rem] -z-10"></div>
         <Container>
           <div className="max-w-md mx-auto text-start md:text-center">
             <SectionBadge title="Features" />
--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -8,7 +8,7 @@ import Iconn from "../../../public/icons/icon.png";
 
 const navbar = () => {
   return (
-    <header className="px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-border z-50">
+    <header className="px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-amber-600/10 z-50">
       <div className="flex items-center justify-between h-full mx-auto md:max-w-screen-xl">
         <div className="flex items-start">
           <Link href="/" className="flex items-center gap-2">
